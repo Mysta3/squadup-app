b20318a1cb61d8f55edfd72b88f54f3a8acb4f6d
Mysta3 <myshawnestallings@gmail.com>
2020-03-18 10:40:16 -0700

    added dynamic navbar

--- squad-up-app-frontend/src/components/Header.js
+++ squad-up-app-frontend/src/components/Header.js
@@ -5,23 +5,32 @@ function Header(props) {
     localStorage.clear();
     props.setStoredUser();
   };
+  console.log(props)
 
-  return (
-    <div className="navBar">
-      <header>
-        <nav>
-          <a href="/squadup/home">Home</a>
-          <a href="/squadup/about">About</a>
-          {props.storedUser && <a href="/squadup/posts/new">New Post</a>}
-          {props.storedUser && (
-            <button onClick={resetStoredName}> Logout</button>
-          )}
-          {!props.storedUser && <a href="/user/signup">Sign Up</a>}
-          {!props.storedUser && <a href="/user/login">Login</a>}
-        </nav>
-      </header>
-    </div>
+  const logged_out_nav = (
+    <header>
+      <nav>
+        <a href="/squadup/home">Home</a>
+        <a href="/squadup/about">About</a>
+        <a href="/squadup/posts/new">New Post</a>
+        <a href="/user/signup">Sign Up</a>
+        <a href="/user/login">Login</a>
+      </nav>
+    </header>
   );
+
+  const logged_in_nav = (
+    <header>
+      <nav>
+        <a href="/squadup/home">Home</a>
+        <a href="/squadup/about">About</a>
+        <a href="/squadup/posts/new">New Post</a>
+        <button onClick={resetStoredName}> Logout</button>
+      </nav>
+    </header>
+  )
+
+  return <div className="navBar"> {props.storedUser ? logged_in_nav: logged_out_nav}</div>
 }
 
 export default Header;
